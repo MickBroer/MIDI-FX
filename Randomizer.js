fde86ea050f175977a60365c8e7b5222653039cc
MickBroer <mickbroerxoxo@gmail.com>
2021-08-11 16:33:26 +0200

    Added random velocity, updated GUI

--- Randomizer.js
+++ Randomizer.js
@@ -1,7 +1,22 @@
+// Initialize variables 
+var chance = 50;
+var minimum = 0;
+var maximum = 127;
+
+// Random velocity + probability gate
 function HandleMIDI(event) {
-	if (Math.random() * 100 < GetParameter("Chance")) {
+	chance = GetParameter("Chance");
+	minimum = GetParameter("Velocity min");
+	maximum = GetParameter("Velocity max")
+	if (event instanceof NoteOn && Math.floor(Math.random() * 100) <= chance) {
+		event.velocity = Math.floor(Math.random() * (maximum - minimum) ) + minimum;
 		event.send();
 	}
 }
 
-var PluginParameters = [{name:"Chance", type:"lin", minValue:1, maxValue:100, numberOfSteps:99, defaultValue:50}];
+// GUI
+var PluginParameters = [
+	{name:"Chance", type:"lin", minValue:0, maxValue:100, numberOfSteps:100, defaultValue:50, unit:"%"},
+	{name:"Velocity min", type:"lin", minValue:0, maxValue:127, numberOfSteps:127, defaultValue:0},
+	{name:"Velocity max", type:"lin", minValue:0, maxValue:127, numberOfSteps:127, defaultValue:127}
+	];
