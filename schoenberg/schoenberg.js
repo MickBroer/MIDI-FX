9cb3ad022a8a3167a1347920fcd68050697ba0bd
MickBroer <mickbroerxoxo@gmail.com>
2023-03-22 12:40:21 +0100

    Update schoenberg.js
    Made sure that the key the user presses is the first midi-note in the twelveTone array

--- schoenberg/schoenberg.js
+++ schoenberg/schoenberg.js
@@ -1,28 +1,38 @@
+/*
+        Schoenberg MIDI effect for Logic pro's Scripter, by Mick Broer
 
+        TO-DO: 
+        - add parameter for rhythm divisions and remove the delay parameter.
+        - add velocity parameters
+        - add octave variations
+        - add recursion parameter
+*/
+var twelveTone = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 function scramble(array) {
     var currentIndex = array.length,  randomIndex;
     while (currentIndex != 0) {
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex--;
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
+      [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
     }
+    array.unshift(0);
 }
 
-twelveTone = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 scramble(twelveTone);
+
 function HandleMIDI(event) {
     var startPitch = event.pitch;
     
     for (var i = 0; i < 12; i++){
         var delay = GetParameter("delay");
         event.pitch = startPitch + twelveTone[i];
-        event.sendAfterMilliseconds(delay * i);
+        var noteOn = new NoteOn(event);
+        noteOn.sendAfterMilliseconds(delay * i);
         NoteOff(event);
     }
 }
 
-function ParameterChanged () {        //Function called when a UI parameter is changed
+function ParameterChanged () {
     if (GetParameter("randomize twelvetone")){
         scramble(twelveTone);
     }
@@ -34,9 +44,9 @@ var PluginParameters = [{
 },
 {
     name: "delay", 
-    type: "lin", 
-    minValue: 0, 
-    maxValue: 10000, 
+    type: "exp", 
+    minValue: 10, 
+    maxValue: 1000, 
     numberOfSteps: 100, 
     defaultValue: 100, 
     unit: "ms"
