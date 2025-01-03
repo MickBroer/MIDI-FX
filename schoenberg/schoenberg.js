b24101b0b3a57631f15047cccbcd5c723296898e
Mick Broer <mickbroerxoxo@gmail.com>
2025-01-03 21:55:46 +0100

    Update schoenberg.js
    remove octave

--- schoenberg/schoenberg.js
+++ schoenberg/schoenberg.js
@@ -2,7 +2,7 @@
         Schoenberg, random 12-tone MIDI effect for Logic pro's Scripter, by Mick Broer
 */
 var NeedsTimingInfo = true;
-var twelveTone = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+var twelveTone = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
 scramble(twelveTone);
 
