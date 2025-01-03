8cdf1d047a481dfa4a868ae8f6111248fa5a7c8b
Mick Broer <mickbroerxoxo@gmail.com>
2025-01-03 22:03:18 +0100

    Update randomizer.js
    Removed defunct key functionality

--- midi-randomizer/randomizer.js
+++ midi-randomizer/randomizer.js
@@ -1,188 +1,196 @@
 /*
-        Randomizer MIDI effect for Logic pro's Scripter, by Mick Broer
-
+    Randomizer MIDI effect for Logic Pro's Scripter, by Mick Broer
 */
+
 function HandleMIDI(event) {
-	chance = 	GetParameter("Overall Chance");
-	chanceC = 	GetParameter("C");
-	chanceCs = 	GetParameter("C#");
-	chanceD = 	GetParameter("D");
-	chanceDs = 	GetParameter("D#");
-	chanceE = 	GetParameter("E");
-	chanceF = 	GetParameter("F");
-	chanceFs = 	GetParameter("F#");
-	chanceG = 	GetParameter("G");
-	chanceGs = 	GetParameter("G#");
-	chanceA = 	GetParameter("A");
-	chanceAs = 	GetParameter("A#");
-	chanceB = 	GetParameter("B");
-	minimum = 	GetParameter("Velocity min");
-	maximum = 	GetParameter("Velocity max");
+    let chance  = GetParameter("Overall Chance");
+    let chanceC  = GetParameter("C");
+    let chanceCs = GetParameter("C#");
+    let chanceD  = GetParameter("D");
+    let chanceDs = GetParameter("D#");
+    let chanceE  = GetParameter("E");
+    let chanceF  = GetParameter("F");
+    let chanceFs = GetParameter("F#");
+    let chanceG  = GetParameter("G");
+    let chanceGs = GetParameter("G#");
+    let chanceA  = GetParameter("A");
+    let chanceAs = GetParameter("A#");
+    let chanceB  = GetParameter("B");
+    let minimum  = GetParameter("Velocity min");
+    let maximum  = GetParameter("Velocity max");
 
-	if (event instanceof NoteOn && Math.floor(Math.random() * 100) <= chance) {
-		event.velocity = Math.floor(Math.random() * (maximum - minimum)) + minimum;
+    // Only randomize NoteOn events
+    if (event instanceof NoteOn && Math.floor(Math.random() * 100) <= chance) {
+        // Randomize velocity
+        event.velocity = Math.floor(Math.random() * (maximum - minimum)) + minimum;
 
-		if 	(probabilityGate(event.pitch, 0, chanceC) 	|| 	probabilityGate(event.pitch, 1, chanceCs) 	|| 
-			probabilityGate(event.pitch, 2, chanceD) 	|| 	probabilityGate(event.pitch, 3, chanceDs)	||
-			probabilityGate(event.pitch, 4, chanceE) 	|| 	probabilityGate(event.pitch, 5, chanceF) 	||
-			probabilityGate(event.pitch, 6, chanceFs)	|| 	probabilityGate(event.pitch, 7, chanceG) 	||
-			probabilityGate(event.pitch, 8, chanceGs) 	|| 	probabilityGate(event.pitch, 9, chanceA) 	||
-			probabilityGate(event.pitch, 10, chanceAs) 	|| 	probabilityGate(event.pitch, 11, chanceB))
-
-			{
-				event.send();
-			}
-	}
-		// make sure every noteOff midi-message is sent
-		else if (event instanceof NoteOff) {
-			event.send();
-	}
+        // Apply probability gate for each pitch class
+        if (probabilityGate(event.pitch,  0, chanceC) ||
+            probabilityGate(event.pitch,  1, chanceCs) ||
+            probabilityGate(event.pitch,  2, chanceD) ||
+            probabilityGate(event.pitch,  3, chanceDs) ||
+            probabilityGate(event.pitch,  4, chanceE) ||
+            probabilityGate(event.pitch,  5, chanceF) ||
+            probabilityGate(event.pitch,  6, chanceFs) ||
+            probabilityGate(event.pitch,  7, chanceG) ||
+            probabilityGate(event.pitch,  8, chanceGs) ||
+            probabilityGate(event.pitch,  9, chanceA) ||
+            probabilityGate(event.pitch, 10, chanceAs) ||
+            probabilityGate(event.pitch, 11, chanceB)) 
+        {
+            event.send();
+        }
+    }
+    // Always forward NoteOff events
+    else if (event instanceof NoteOff) {
+        event.send();
+    }
 }
 
 function probabilityGate(pitch, note, notechance) {
-	return (pitch % 12 == note && Math.floor(Math.random() * 100) <= notechance) ? true : false;
+    return (pitch % 12 === note && Math.floor(Math.random() * 100) <= notechance);
 }
 
-// GUI
-var PluginParameters = [{
-	name:"Key", 
-	type:"Menu",
-	valueStrings: ["No key",
-	"C", "Cm", 
-	"C#", "C#m", 
-	"D", "Dm", 
-	"D#", "D#m", 
-	"E", "Em", 
-	"F", "Fm", 
-	"F#", "Fm", 
-	"G", "Gm", 
-	"G#", "G#m", 
-	"A", "Am", 
-	"A#", "A#m", 
-	"B", "Bm"],
-	defaultValue:0
-}, {
-	name:"- Probability gate -",
-	type:"text"
-},	{
-	name:"Overall Chance", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:100, 
-	unit:"%"
-}, 	{
-	name:"C", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"C#", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"D", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"D#", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"E", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"F", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"F#", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"G", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"G#", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"A", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"A#", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"B", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:100, 
-	numberOfSteps:100, 
-	defaultValue:50, 
-	unit:"%"
-},	{
-	name:"----- Velocity -----",
-	type:"text"
-},	{
-	name:"Velocity min", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:127, 
-	numberOfSteps:127, 
-	defaultValue:0
-},	{
-	name:"Velocity max", 
-	type:"lin", 
-	minValue:0, 
-	maxValue:127, 
-	numberOfSteps:127, 
-	defaultValue:127
-}];
+var PluginParameters = [
+    {
+        name: "- Probability gate -",
+        type: "text"
+    },
+    {
+        name: "Overall Chance", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 100, 
+        unit: "%"
+    },
+    {
+        name: "C", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "C#", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "D", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "D#", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "E", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "F", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "F#", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "G", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "G#", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "A", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "A#", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "B", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 100, 
+        numberOfSteps: 100, 
+        defaultValue: 50, 
+        unit: "%"
+    },
+    {
+        name: "----- Velocity -----",
+        type: "text"
+    },
+    {
+        name: "Velocity min", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 127, 
+        numberOfSteps: 127, 
+        defaultValue: 0
+    },
+    {
+        name: "Velocity max", 
+        type: "lin", 
+        minValue: 0, 
+        maxValue: 127, 
+        numberOfSteps: 127, 
+        defaultValue: 127
+    }
+];
