db3031f69b49396d948c39f13ebbcc56f2b9236b
Mick Broer <mickbroerxoxo@gmail.com>
2025-01-03 22:36:35 +0100

    added a scrape midi effect

---
+++ scrape/scrape.js
@@ -0,0 +1,88 @@
+/*
+        Scrape MIDI effect for Logic pro's Scripter, by Mick Broer
+*/
+
+var NeedsTimingInfo = true;
+
+function HandleMIDI(event) {
+    if (event instanceof NoteOn) {
+        let repeats    = GetParameter("Number of Repeats");
+        let startVel   = GetParameter("Start Velocity");
+        let endVel     = GetParameter("End Velocity");
+        let timeIndex  = GetParameter("Time");
+
+        let velocityStep = 0;
+        if (repeats > 1) {
+            velocityStep = (endVel - startVel) / (repeats - 1);
+        }
+
+        let beatDelay = getTime(timeIndex);
+
+        for (let i = 0; i < repeats; i++) {
+            let stepVel = Math.round(startVel + i * velocityStep);
+
+            if (stepVel < 1)   stepVel = 1;
+            if (stepVel > 127) stepVel = 127;
+
+            let noteOn = new NoteOn(event);
+            noteOn.velocity = stepVel;
+
+            noteOn.sendAfterBeats(i * beatDelay);
+
+            let noteOff = new NoteOff(noteOn);
+
+            noteOff.sendAfterBeats(i * beatDelay + 0.9 * beatDelay);
+        }
+    }
+
+    else if (event instanceof NoteOff) {
+        event.send();
+    }
+}
+
+function getTime(index) {
+    switch (index) {
+    	case 0: return 0.015625; // 1/32
+        case 1: return 0.03125; // 1/32
+        case 2: return 0.0625;  // 1/16
+        case 3: return 0.125;   // 1/8
+        case 4: return 0.25;    // 1/4
+        case 5: return 0.5;     // 1/2
+        default:
+            Trace("Error in getTime()");
+            return 0.125;       // 1/8
+    }
+}
+
+var PluginParameters = [
+    {
+        name: "Number of Repeats",
+        type: "lin",
+        minValue: 1,
+        maxValue: 64,
+        numberOfSteps: 63,
+        defaultValue: 8
+    },
+    {
+        name: "Start Velocity",
+        type: "lin",
+        minValue: 1,
+        maxValue: 127,
+        numberOfSteps: 126,
+        defaultValue: 40
+    },
+    {
+        name: "End Velocity",
+        type: "lin",
+        minValue: 1,
+        maxValue: 127,
+        numberOfSteps: 126,
+        defaultValue: 100
+    },
+    {
+        name: "Time",
+        type: "menu",
+        valueStrings: ["1/64", "1/32", "1/16", "1/8", "1/4", "1/2"],
+        defaultValue: 2
+    }
+];
